10be924163baa30b9d26c9fdb75c7a7c042aaa75
Andrés de la Cuesta López <48914050+ancues02@users.noreply.github.com>
2021-11-15 11:47:18 +0100

    sigue sin leer bien el mapa

--- Practica3/src/main.cpp
+++ Practica3/src/main.cpp
@@ -9,7 +9,9 @@
 
 const int nameSize = 16;
 
-int** vecMaps = nullptr;
+int** vecMaps = nullptr;//64x64 informacion del mapa
+char* vecNamesMaps = nullptr;//nombre de cada mapa
+int* playerStart = nullptr;//2 x 1, posicion del jugador y su orientacion
 
 float toRad(float deg) {
 	return (deg * 3.14) / 180;
@@ -21,23 +23,57 @@ void lecturaMapa(const char* fileName) {
 	FILE* file = Platform::OpenFile(fileName, "r");
 
 	if (file == nullptr) return; // Ha fallado la carga
-	int tile, numMaps;
+
+	uint16_t tile, player, numMaps;
+	
 	//numero de mapas
 	if (fread(&numMaps, 2, 1, file) == 0) return;
 	if (bigEndian)
-		numMaps = FLIPENDIAN_32((int)numMaps);
+		numMaps = FLIPENDIAN_16((int)numMaps);
 	vecMaps = new int* [numMaps];
-
-	char name[nameSize];
-	if (fread(&name, 16, 1, file) == 0) return;
+	vecNamesMaps = new char[numMaps];
+	playerStart = new int[2];//posicion y orientacion del player
 
 	for (int x = 0; x < numMaps && !feof(file); ++x) {
-		vecMaps[x] = new int[64 * 64];
-		for (int y = 0; y < 64 * 64; ++y) {
+		//nombre del mapa
+		char name[nameSize];
+		if (fread(&name, 16, 1, file) == 0) return;
+		vecNamesMaps[x] = *name;
+
+		//el mapa como tal, 2 planos de 64*64 el primero con informaicon del nivel
+		// un tile con valor i se pinta en 2*i -1
+		//el segundo con la informacion del player, solo quedarse con el valor entre 19 y 22 inclusives
+		//19 player mira norte, 20 este, 21, sur, 22 oeste
+
+		vecMaps[x] = new int[ 64 * 64];
+		//primer plano, informacion del nivel (paredes)
+		for (int y = 0; y < 64 * 64 ; ++y) {
 			if (fread(&tile, 2, 1, file) == 0) break;
 			if (bigEndian) tile = FLIPENDIAN_16(tile);
 			vecMaps[x][y] = tile;
+			if (tile <= 63 && tile >0) {
+				//std::cout << "tile " << tile << std::endl;
+
+			}
+			else
+				std::cout << "tile " << tile << std::endl;
+
 		}
+		//segundo plano, informacion del jugador (posicion inicial y donde mira)
+		for (int y = 0; y < 64 * 64 ; ++y) {
+			if (fread(&player, 2, 1, file) == 0) break;
+			if (bigEndian) player = FLIPENDIAN_16(player);
+			if (player >= 19 && player <= 22) {
+				playerStart[0] = y;
+				playerStart[1] = player;
+				break;
+			}
+			else
+				std::cout << "player " << player << std::endl;
+
+		}
+		std::cout << "player " << player << std::endl;
+
 	}
 }
 
@@ -97,6 +133,7 @@ int main(int argc, char* argv[])
 	//auto start = std::chrono::high_resolution_clock::now();
 
 	Renderer::ReadImage("assets/walls.pak");
+	lecturaMapa("assets/maps.pak");
 	while (Platform::Tick() /* && cFrames++ < totalFrames*/)
 	{
 		Renderer::Clear({ 255, 0, 0, 0 });//limpiamos a color negro por defecto
